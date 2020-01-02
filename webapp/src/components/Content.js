0d903b49c33da899c7857b8bc899909d3faaaffd
Josh Smith <jsmith@Joshs-MacBook-Pro.local>
2020-01-01 21:08:28 -0600

    Select Date

--- webapp/src/components/Content.js
+++ webapp/src/components/Content.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { Typography, Grid, Switch, FormGroup, FormControlLabel} from '@material-ui/core';
+import { Typography, Grid, Switch } from '@material-ui/core';
+import DateFnsUtils from '@date-io/date-fns';
+import {  MuiPickersUtilsProvider, KeyboardDatePicker } from '@material-ui/pickers';
 import DeckGL from '@deck.gl/react';
 import * as d3 from "d3";
 import {json as requestJson, csv as requestCsv} from 'd3-request';
@@ -7,19 +9,10 @@ import {GeoJsonLayer} from '@deck.gl/layers';
 import {StaticMap} from 'react-map-gl';
 import { makeStyles } from '@material-ui/core/styles';
 import AustinData from '../data/austin_airbnb.geojson';
+import AvailabilityData from '../data/austin_availability_by_neighborhood.csv';
+import {neighbourhoods, dates, colors} from '../data/austin_arrays';
 
 const MAPBOX_TOKEN = 'pk.eyJ1IjoiY3NlNjI0MiIsImEiOiJjazIyN3dodWQxcjhqM2ltbGNrY2Z3NzVtIn0.TYBkWk-HsUdkHkVkYyXE9A';
-const colors =[[230, 25, 75], [60, 180, 75], [255, 225, 25], [0, 130, 200], [245, 130, 48], [145, 30, 180], 
-[70, 240, 240], [240, 50, 230], [210, 245, 60], [250, 190, 190], [0, 128, 128], [230, 190, 255], [170, 110, 40], 
-[255, 250, 200], [128, 0, 0], [170, 255, 195], [128, 128, 0], [255, 215, 180], [0, 0, 128], [128, 128, 128], [255, 255, 255], [0, 0, 0],
-[230, 25, 75], [60, 180, 75], [255, 225, 25], [0, 130, 200], [245, 130, 48], [145, 30, 180], 
-[70, 240, 240], [240, 50, 230], [210, 245, 60], [250, 190, 190], [0, 128, 128], [230, 190, 255], [170, 110, 40], 
-[255, 250, 200], [128, 0, 0], [170, 255, 195], [128, 128, 0], [255, 215, 180], [0, 0, 128], [128, 128, 128], [255, 255, 255], [0, 0, 0],
-[230, 25, 75], [60, 180, 75], [255, 225, 25], [0, 130, 200]];
-
-const neighbourhoods = ["78739", "78754", "78732", "78737", "78756", "78747", "78751", "78712", "78738", "78725", "78757", "78728", "78744", 
-    "78736", "78702", "78741", "78719", "78742", "78722", "78703", "78717", "78749", "78730", "78745", "78731", "78748", "78729", "78752", 
-    "78724", "78704", "78753", "78705", "78758", "78746", "78759", "78726", "78733", "78723", "78750", "78735", "78721", "78727", "78734", "78701"];
 
 const color_scale = d3.scaleOrdinal().domain(neighbourhoods).range(colors);
 
@@ -31,6 +24,8 @@ const useStyles = makeStyles(theme => ({
         height : "100%"
     }
 }));
+
+let handleSimulation;
 
 const viewState = {
     width: window.innerWidth,
@@ -47,6 +42,10 @@ class Content extends React.Component {
         super(props);
         this.state = {
             austinData : null,
+            availability : null,
+            selectedTime : 0,
+            simulate: false,
+            filter: false,
             elevationByCount : false
         }
     };
@@ -57,19 +56,85 @@ class Content extends React.Component {
               this.loadGeo(response);
             }    
         });
+
+        requestCsv(AvailabilityData, (error, response) => {
+            if (!error) {
+                this.loadAvailability(response);            }
+        })
     }
 
     loadGeo = response => {
         this.setState({austinData : response});
-        let neighbourhoods = [];
-        for (let i = 0; i < response.features.length; i++) {
-            neighbourhoods.push(response.features[i].properties.neighbourhood);
-        }
-        console.log(neighbourhoods);
+    }
+
+    loadAvailability = response => {
+        this.setState({availability : response});
     }
     
+    simulateTime() {
+        const {availability} = this.state;
+        const self = this;
+        let i = this.state.selectedTime;
+        handleSimulation = setInterval(function () {     
+            if (i == availability.length) {          	  
+                i = 0;
+            }
+            self.setState({selectedTime : i}, () => {
+                i++;
+            })
+         }, 1500);
+
+    }
+
+    filterData(data) {
+        let filteredData;
+        const {selectedTime} = this.state;
+        if (data !== null) {
+            filteredData = data.filter(function(row) {
+                return row.date == dates[selectedTime];
+            })
+
+            // count is the combined count of available listings across all neighborhoods for the selected date
+            let count = 0;
+            filteredData.forEach(function(d) {
+                count += parseInt(d.count);
+            });
+
+            // represents the total count of available listings for the selected date
+            return filteredData;
+        }
+    }
+
+    formatDate(date) {
+        var d = new Date(date),
+            month = '' + (d.getMonth() + 1),
+            day = '' + d.getDate(),
+            year = d.getFullYear();
+    
+        if (month.length < 2) 
+            month = '0' + month;
+        if (day.length < 2) 
+            day = '0' + day;
+    
+        return [year, month, day].join('-');
+    }
+
+    handleTimeChange = date => {
+        clearInterval(handleSimulation);
+        console.log(this.state.selectedTime);
+        this.setState((state) => ({filter : true, selectedTime : dates.indexOf(this.formatDate(date))}),
+            () => {
+                if (this.state.simulate) {
+                this.simulateTime();
+                }
+            }   
+        );
+    }
+
     renderLayers() {
-        const {austinData, elevationByCount} = this.state;
+        const {austinData, availability, elevationByCount, simulate, filter} = this.state;
+        let filteredData = filter ? this.filterData(availability) : null;
+
         return [ 
             new GeoJsonLayer({
             id: 'geojson-layer',
@@ -86,8 +151,40 @@ class Content extends React.Component {
             },
             getRadius: 100,
             getLineWidth: 1,
-            getElevation: d => {return elevationByCount ? parseInt(d.properties.count) : parseInt(d.properties.avg_price)},
-            updateTriggers: {getElevation: this.state.elevationByCount}
+            getElevation: d => {
+                if (this.state.elevationByCount) {
+                    let tempData = {};
+                    if (filter) {
+                        tempData = filteredData.filter(
+                            function(row) {
+                                return row.neighbourhood == d.properties.neighbourhood;
+                            }
+                        )[0];
+                        if (tempData) {
+                            return parseInt(tempData.count) * 10;
+                        }
+                    } else {
+                        return parseInt(d.properties.count);
+                    }
+                } else {
+                    let tempData = {};
+                    if (filter) {
+                        tempData = filteredData.filter(
+                            function(row) {
+                                return row.neighbourhood == d.properties.neighbourhood;
+                            }
+                        )[0];
+                        if (tempData) {
+                            return parseInt(tempData.avg_price) * 10;
+                        }
+                    } else {
+                        return parseInt(d.properties.avg_price) * 5;
+                    }
+                }
+            },
+            updateTriggers: {
+                getElevation: [this.state.elevationByCount, this.state.selectedTime]
+            }
             // onHover: ({object, x, y}) => {
             //     const tooltip = object.properties.name || object.properties.station;
             //     /* Update tooltip
@@ -105,7 +202,7 @@ class Content extends React.Component {
                 <Grid container style={{height :  "100%"}}>
                     <Grid style={{height :  "100%"}} item xs={3}>
                         <Typography component="div">
-                            <Grid component="label" container justify="center" spacing={1}>
+                            <Grid style={{height :  "100%"}} component="label" container justify="center" spacing={1}>
                                 <Grid item>Average Price</Grid>
                                 <Grid item>
                                     <Switch size="small" checked={this.state.elevationByCount} 
@@ -113,6 +210,25 @@ class Content extends React.Component {
                                 </Grid>
                                 <Grid item>Number of Listings</Grid>
                             </Grid>
+                            <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                                <Grid container justify="space-around">
+                                    <KeyboardDatePicker
+                                        disableToolbar
+                                        variant="inline"
+                                        format="MM/dd/yyyy"
+                                        minDate = "09/19/2019"
+                                        maxDate = "09/17/2020"
+                                        margin="normal"
+                                        id="date-picker-inline"
+                                        label="Date picker inline"
+                                        onChange={this.handleTimeChange}
+                                        value={new Date((dates[this.state.selectedTime]) + " 12:00:00")}
+                                        KeyboardButtonProps={{
+                                            'aria-label': 'change date',
+                                        }}
+                                    />
+                                </Grid>
+                            </MuiPickersUtilsProvider>
                         </Typography>
                     </Grid>
                     <Grid style={{height :  "100%"}} item xs={9}>
